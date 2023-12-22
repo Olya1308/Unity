717e0e001d5ba4751642ac6aecad1e2588325b5a
Olya <107269881+Olya1308@users.noreply.github.com>
2023-12-22 21:45:51 +0200

    dz_2(first)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10; // швидк≥сть
+
+    public CharacterController characterController; //€к саме рухаЇмо гравц€
+
+    private Vector3 move; //рух гравц€
+
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal"); // рух вл≥во вправо
+        float z = Input.GetAxis("Vertical"); // вперед назад
+
+        move = transform.right * x + transform.forward * z; //напр€м руху
+
+        characterController.Move(move * speed * Time.deltaTime);// контрoлер руху
+
+        Debug.Log(x + " " + z);
+
+    }
+}
