2d7035513131c4c4650ee2019e63b3a1bc337902
Olya <107269881+Olya1308@users.noreply.github.com>
2023-12-30 19:14:36 +0200

    dz_2_with_question

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,9 +10,24 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 move; //рух гравц€
 
+    public float gravity = -10; //сила прит€ж≥нн€
+
+    public float JumpHeight = 5; //сила (або висота) стрибка 
+
+    private Vector3 velocity; //напр€м + швидк≥сть земного т€ж≥нн€
+
+    public bool isGrounded; //ми на земл≥ чи н≥?
+
+    public Transform CheckGround; //положенн€ н≥жок гравц€
+
+    public LayerMask groundLayer; //на основ≥ слою ми створили зм≥нну
+
 
     // Start is called before the first frame update
-    void Start() {}
+    void Start() 
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,7 +39,32 @@ public class PlayerController : MonoBehaviour
 
         characterController.Move(move * speed * Time.deltaTime);// контрoлер руху
 
-        Debug.Log(x + " " + z);
+        isGrounded = Physics.CheckSphere(CheckGround.position, 0.1F, groundLayer);
 
+        Debug.Log(velocity.y);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f; // нас т€гне вниз
+        }
+
+        if(isGrounded) // €кщо ми на земл≥ ...
+        {            
+            if (Input.GetButtonDown("Jump")) //та нитискаЇмо на стрибати (Space)
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = JumpHeight; // швидк≥сть руху по ” координат≥ == сил≥ стрибка
     }
 }
